920ad41229f7becbad54e8a321b1881b25034974
sanderVE <sandervanelderen@gmail.com>
2016-04-01 14:59:06 +0200

    Webshop klasse aangemaakt

---
+++ Dierenasiel/Sellable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dierenasiel
+{
+    public class ISellable
+    {
+        private String name;
+        private decimal price;
+
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public decimal Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+    }
+}
